cb7efc41a56507546fdc288cf4e3f3077fed940b
zainabnoori <zainab.noori@and.digital>
2023-08-07 16:12:11 +0100

    create app

---
+++ src/config/auth.ts
@@ -0,0 +1,13 @@
+export const AwsConfigAuth = {
+    region: "eu-west-2",
+    userPoolId: "eu-west-2_3s4hIWI0v",
+    userPoolWebClientId: "236iupbnnik0a9c4oi5nr2cnhi",
+    cookieStorage: {
+        domain: "localhost",
+        path: "/",
+        expires: 365,
+        sameSite: "strict",
+        secure: true,
+    },
+    authenticationFlowType: "USER_SRP_AUTH",
+};
