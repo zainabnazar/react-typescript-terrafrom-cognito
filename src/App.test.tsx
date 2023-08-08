1fc5d21fe5476715f9f674410cf637adbcab95b0
zainabnoori <zainab.noori@and.digital>
2023-08-08 12:31:33 +0100

    Update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // For enhanced DOM assertions
-import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect'; 
 import App from './App';
 
 describe('App Component', () => {
